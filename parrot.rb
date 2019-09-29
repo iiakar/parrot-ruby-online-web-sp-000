d62894c0e4ded53f7e59fee375ec3bdabb73d42a
Ibrahim  Akar <ibsakar@gmail.com>
2019-09-29 19:24:07 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(phrase,*"Squawk!")
+  puts "#{phrase}"
+end 
+return parrot 
+#parrot("Pretty Bird!")
+
+ 
+
+    
